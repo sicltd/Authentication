9dca042abc34d0b054110cdfc42b0a0c14cb7017
Paul Montenero <pmontenero@me.com>
2014-03-27 20:59:53 -0400

    Updated Database Connection

--- Authentication/Controllers/SecurityController.cs
+++ Authentication/Controllers/SecurityController.cs
@@ -10,27 +10,27 @@ namespace Authentication.Controllers
 {
     public class SecurityController : ApiController
     {
-        [HttpGet]
-        public bool Register(string emailAddress, string password)
-        {
-            using (var context = new AuthenticationEntities())
-            {
-                var user = (from x in context.Users
-                            where x.EmailAddress == emailAddress
-                            select x).FirstOrDefault();
-                if (user != null)
-                {
-                    throw new Exception("A user With This Email Address Already Exists");
-                }
+        //[HttpGet]
+        //public bool Register(string emailAddress, string password)
+        //{
+        //    using (var context = new AuthenticationEntities())
+        //    {
+        //        var user = (from x in context.Users
+        //                    where x.EmailAddress == emailAddress
+        //                    select x).FirstOrDefault();
+        //        if (user != null)
+        //        {
+        //            throw new Exception("A user With This Email Address Already Exists");
+        //        }
 
-                var newuser = new User();
-                newuser.EmailAddress = emailAddress;
-                newuser.Password = password;
-                context.Users.Add(newuser);
-                context.SaveChanges();
-                return true;
-            }
-        }
+        //        var newuser = new User();
+        //        newuser.EmailAddress = emailAddress;
+        //        newuser.Password = password;
+        //        context.Users.Add(newuser);
+        //        context.SaveChanges();
+        //        return true;
+        //    }
+        //}
 
 
         [HttpGet]
